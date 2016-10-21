17904af95ee917cc152155674dc2c21fa971d4d2
Dong Liang <dongliang3571@gmail.com>
2016-10-21 00:28:12 -0400

    added base

--- fintech/home/views.py
+++ fintech/home/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    template_name = "index.html"
-    return render(request, "index.html")
+    template_name = "home/index.html"
+    return render(request, template_name)
